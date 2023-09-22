354a42c18c87f302a2b81c07e9bc026ea342e2d3
Yuta Mukai <mukai.yuta@fujitsu.com>
2023-09-22 13:56:28 +0000

    fixup! [ModuloSchedule] Implement modulo variable expansion for pipelining

--- llvm/lib/Target/PowerPC/PPCInstrInfo.cpp
+++ llvm/lib/Target/PowerPC/PPCInstrInfo.cpp
@@ -5574,6 +5574,13 @@ public:
     return TripCount > TC;
   }
 
+  void createRemainingIterationsGreaterCondition(
+      int TC, MachineBasicBlock &MBB, SmallVectorImpl<MachineOperand> &Cond,
+      DenseMap<unsigned, unsigned> RegMap) override {
+    llvm_unreachable(
+        "Target didn't implement createRemainingIterationsGreaterCondition");
+  }
+
   void setPreheader(MachineBasicBlock *NewPreheader) override {
     // Do nothing. We want the LOOP setup instruction to stay in the *old*
     // preheader, so we can use BDZ in the prologs to adapt the loop trip count.
@@ -5598,6 +5605,8 @@ public:
     // Ensure the loop setup instruction is deleted too.
     LoopCount->eraseFromParent();
   }
+
+  bool isMVEExpanderSupported() override { return false; }
 };
 } // namespace
 
